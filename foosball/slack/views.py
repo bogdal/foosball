be8be0ee010a493127d5c43820e66b1a325c1ba6
Adam Bogdał <adam@bogdal.pl>
2016-07-07 18:22:15 +0200

    Improve messages

--- foosball/slack/views.py
+++ foosball/slack/views.py
@@ -9,7 +9,7 @@ except ImportError:
 
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 
@@ -19,6 +19,7 @@ from .models import SlackTeam
 
 SLACK_AUTH_URL = 'https://slack.com/oauth/authorize'
 SLACK_ACCESS_URL = 'https://slack.com/api/oauth.access'
+SLACK_POST_URL = 'https://slack.com/api/chat.postMessage'
 
 
 def oauth_callback(request):
@@ -59,38 +60,59 @@ def oauth_callback(request):
 def slack(request):
     data = json.loads(request.POST.get('payload', '{}')) or request.POST
     if data.get('token') == settings.SLACK_VERIFICATION_TOKEN:
+        color = '#3AA3E3'
         message = {
-            "response_type": "in_channel",
-            "text": "<!here> Who wants to play :soccer:?",
-            "attachments": [{
-                "fallback": "You are unable to play",
-                "callback_id": "foosball_game",
-                "color": "#3AA3E3",
-                "attachment_type": "default",
-                "actions": [{
-                    "name": "Add me",
-                    "text": "Add me",
-                    "type": "button",
-                    "value": "add"}]}]}
+            'response_type': 'in_channel',
+            'text': '<!here> Who wants to play :soccer:?',
+            'attachments': [{
+                'fallback': 'You are unable to play',
+                'callback_id': 'foosball_game',
+                'color': color,
+                'attachment_type': 'default',
+                'actions': [{
+                    'name': 'Add me',
+                    'text': 'Add me',
+                    'type': 'button',
+                    'value': 'add'}]}]}
 
-        def get_players(items, link_names=True):
+        def get_players(items, link_names=False):
             display = '<@%s>' if link_names else '@%s'
             return ', '.join(map(lambda x: display % x, items))
 
         for action in data.get('actions', []):
-            desc = data['original_message']['attachments'][0].get('text', '')
-            players = re.findall('(?<=@)\w+', desc)
+            fields = data['original_message']['attachments'][0].get('fields')
+            value = fields[0]['value'] if fields else ''
+            players = re.findall('(?<=@)\w+', value)
             if action['value'] == 'add':
-                if data['user']['name'] not in players:
-                    players.append(data['user']['name'])
-                message['attachments'][0]['text'] = 'Players: %s' % (
-                    get_players(players, link_names=False),)
+                if data['user']['name'] in players:
+                    return HttpResponse()
+                players.append(data['user']['name'])
+                message['attachments'][0]['fields'] = [{
+                    'title': 'Players',
+                    'value': get_players(players)}]
+
             if len(players) == 4:
                 random.shuffle(players)
+                team = SlackTeam.objects.get(team_id=data['team']['id'])
+                new_message = {
+                    'token': team.access_token,
+                    'response_type': 'in_channel',
+                    'channel': data['channel']['id'],
+                    'attachments': json.dumps([{
+                        'text': 'Alright, let\'s play :zap:',
+                        'fields': [{
+                            'title': 'Team 1',
+                            'value': get_players(players[:2]),
+                            'short': True}, {
+                            'title': 'Team 2',
+                            'value': get_players(players[2:]),
+                            'short': True}],
+                        'color': color}])}
+                requests.post(SLACK_POST_URL, data=new_message)
+
                 message = {
-                    "response_type": "in_channel",
-                    'text': "Let's play %s :vs: %s" % (
-                        get_players(players[:2]),
-                        get_players(players[2:]))}
-        return JsonResponse(message)
+                    'response_type': 'in_channel',
+                    'text': 'Team collected :+1:'}
+        requests.post(data['response_url'], data=json.dumps(message))
+        return HttpResponse()
     return redirect('/')
